b7952320a54e9a5a4ca9ebf083a2f1c22dd66b22
Arif Canpol <canpolska@gmail.com>
2022-05-04 11:12:43 +0200

    added before after methods, editted Runner class and added FailedRunner

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,4 +1,33 @@
 package hooks;
 
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
 public class Hooks {
+
+    @Before(value = "@player_page")
+    public void navigateToPlayerPage(){
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("player_url"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+
+        if (scenario.isFailed()) {
+            final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+
+        Driver.closeDriver();
+
+    }
 }
+
--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -2,19 +2,18 @@ package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "pretty",
                 "html:target/default-cucumber-reports.html",
-                "json:target/json-reports/cucumber.json",
+                "json:target/json-report/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"
         },
-        features = "./src/test/resources/features",
-        glue = {"stepdefinitions","hooks"},
-        tags = "@player",
+        features = "@target/failedRerun.txt",
+        glue = "stepdefinitions",
         dryRun = false
 )
-public class Runner {
+public class FailedRunner {
 }
